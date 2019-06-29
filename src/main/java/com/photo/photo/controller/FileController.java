b254a8227f696951e7b23df9ef50d3423a8f164e
leoncul8r <953733547@qq.com>
2019-06-29 15:48:48 +0800

    leonc

--- src/main/java/com/photo/photo/controller/FileController.java
+++ src/main/java/com/photo/photo/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.photo.photo.controller;
 
 import com.drew.imaging.ImageProcessingException;
+import com.google.gson.JsonObject;
 import com.photo.photo.entity.Photo;
 import com.photo.photo.service.PhotoService;
 import com.photo.photo.utils.PhotoClassify;
@@ -14,8 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestController
@@ -34,9 +33,9 @@ public class FileController
     @Autowired
     private PhotoService photoService;
 
-    @PostMapping ("/upload")
     @ResponseBody
-    public String uploadPhoto(@RequestParam("file") MultipartFile file) throws IOException, ImageProcessingException
+    @RequestMapping ("/upload")
+    public JsonObject uploadPhoto(@RequestParam("file") MultipartFile file) throws IOException, ImageProcessingException
     {
         Photo photo = new Photo ();
         photoService.save (photo);
@@ -44,7 +43,9 @@ public class FileController
         if (result.equals ("上传失败，请选择文件") || result.equals ("错误的文件格式") || result.equals ("上传失败！"))
         {
             photoService.delete (photo);
-            return result;
+            JsonObject res = new JsonObject();
+            res.addProperty("上传结果", result);
+            return res;
         }
         else
         {
@@ -54,22 +55,27 @@ public class FileController
             ThumbnailsMake.Make(150,150, path, path + th, result) ;
             //↑生成缩略图
 
-            return  "上传成功! " + "识别为：" + tag;
+            JsonObject res = new JsonObject();
+            res.addProperty ("上传结果", "上传成功");
+            res.addProperty ("tag", tag);
+            return res;
         }
     }
 
-    @PostMapping("/multiUpload")
     @ResponseBody
-    public String multiUploadPhoto(HttpServletRequest request) throws IOException, ImageProcessingException
+    @RequestMapping("/multiUpload")
+    public JsonObject multiUploadPhoto(HttpServletRequest request) throws IOException, ImageProcessingException
     {
+        JsonObject res = new JsonObject();
+
         Photo photo = new Photo ();
         photoService.save (photo);
         String result = PhotoUpload.multiUpload(request, photo);     //若上传成功，result为文件名;失败则为报错信息。
-        List<String> tags = new ArrayList<> ();
         if (!result.contains ("!"))
         {
             photoService.delete (photo);
-            return result;
+            res.addProperty("上传结果", result);
+            return res;
         }
         else
         {
@@ -81,19 +87,15 @@ public class FileController
 //                    log.info (resultCut[i]);
                     String tag = PhotoClassify.Classify (path + resultCut[i]);             //通过百度云图像识别获取标签
                     log.info(tag + resultCut[i]);
-                    tags.add(tag);
+                    res.addProperty ("tag" + (i + 1), tag);
                     photoService.save (photo,photo.getPhotoId (),result,tag,userId);     //存储照片信息到数据库
                     ThumbnailsMake.Make(150,150, path, path + th, resultCut[i]) ;
                     //↑生成缩略图
                 }
             }
         }
-        String tagsString = "";
-        for(int i = 0; i < tags.size();i++)
-        {
-            tagsString += ("\n图" + (i + 1) + "识别为" + tags.get (i));
-        }
-        return "上传成功! " + tagsString;
+        res.addProperty ("上传结果", "上传成功");
+        return res;
     }
 
 }
