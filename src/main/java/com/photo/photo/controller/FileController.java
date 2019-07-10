2f7c3dd8a764d3f467277dd1f003e800dc8c9cb6
leoncul8r <953733547@qq.com>
2019-07-10 17:20:56 +0800

    leonc

--- src/main/java/com/photo/photo/controller/FileController.java
+++ src/main/java/com/photo/photo/controller/FileController.java
@@ -10,6 +10,7 @@ import com.photo.photo.utils.UserIdFromRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,7 +23,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping ("/file")
-//@EnableAsync
+@EnableAsync
 public class FileController
 {
     private static final Logger log = LoggerFactory.getLogger(FileController.class);
